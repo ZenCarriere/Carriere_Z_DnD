e72ef2023e252c46cdc26cdd6d94168d1354c99a
ZenCarriere <59712949+ZenCarriere@users.noreply.github.com>
2020-06-05 12:15:41 -0400

    presentation

--- script/app.js
+++ script/app.js
@@ -1,6 +1,8 @@
 (() => {
 	// set up the puzzle pieces and boards
 	const puzzleButtons = document.querySelectorAll('#buttonHolder img'),
+		puzzlePieces = document.querySelectorAll(".puzzle-image"),
+		dropZones = document.querySelectorAll(".drop-zone"),
 		gameBoard = document.querySelector(".puzzle-board");
 
 
@@ -12,11 +14,47 @@
 		gameBoard.style.backgroundImage = `url(images/backGround${this.dataset.bgkey}.jpg)`;
 	}
 
+	function allowDrag(event) {
+		console.log('started dragging an image: this one - ', event.target.id);
+
+		event.dataTransfer.setData("draggedImg",this.id);
+	}
+
+	function allowDragOver(event){
+		event.preventDefault();
+		console.log('dragged something over me!');
+	}
+
+	function allowDrop(event){
+		console.log('dropped something on me');
+
+		let droppedImage = event.dataTransfer.getData("draggedImg");
+
+		event.target.appendChild(document.querySelector(`#${droppedImage}`));
+
+	}
+
 	// add event handling here -> how is the user going to use our app?
 	// what triggers do we need?
 
 	// click on the bottom buttons to change the puzzle image we're working with
 	puzzleButtons.forEach(button => button.addEventListener('click', changeImageSet));
+
+	puzzlePieces.forEach(piece => piece.addEventListener('dragstart', allowDrag));
+
+	dropZones.forEach(zone => {
+		zone.addEventListener('dragover', allowDragOver);
+		zone.addEventListener('drop', allowDrop);
+		});
+
+	// rewrite loop in old style
+	// for (let zone in dropZones) {
+	// 	zone.addEventListener('dragover', allowDragOver);
+	// 	zone.addEventListener('drop', allowDrop);
+	// }
+
+
+
 
 	//research call, apply, and bind
 	changeImageSet.call(puzzleButtons[0]);
